2255101580aa5fede4f767e6b8a2ad9013f934c5
Delgadoc86 <delgadoc1986@hotmail.com>
2023-07-12 22:17:57 -0300

    for array

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+     int valores[]= {10, 20 ,30, 40 ,50};
+                     //0 < 5
+                     //1 < 5
+                     //2 < 5
+                     //3 < 5
+                     //4 < 5
+                     //5 < 5   ¡ESTE NO!
+
+
+        for (int i = 0; i < valores.length; i++) {
+
+            System.out.println (valores[i]);
+        } System.out.println("acá terminó el ejercicio");
+
+    }
+}
